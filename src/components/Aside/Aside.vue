a77763b79ab4ab59c0895d585cab3763ae02ee35
Bruno Lima <brunolima.cad@gmail.com>
2023-08-24 06:41:48 -0600

    implemented history store

--- src/components/Aside/Aside.vue
+++ src/components/Aside/Aside.vue
@@ -1,9 +1,16 @@
 <script setup>
+import { useHistoryStore } from '../../stores/history'
+
+const { history } = useHistoryStore()
 </script>
 
 <template>
-  <div id="asideContainer" class="flex-1">
-    <p>This is the aside</p>
+  <div id="asideContainer" class="flex-1 px-4 py-10">
+    <h3 class="text-xl mb-4">Search History</h3>
+    <div v-if="!history.length">No search history</div>
+    <div v-for="search of history" :key="search.id">
+      {{ search.firstCurrency }} to {{ search.secondCurrency }} - {{ search.dateTime }}
+    </div>
   </div>
 </template>
 
